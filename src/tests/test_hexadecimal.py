e6c16a854ced02c55684752cba53da4f1f0298eb
Mahfuzul Amin <mahfuzul.amin@capgemini.com>
2021-04-23 05:58:04 +0000

    Fixed unit test error

--- src/tests/test_hexadecimal.py
+++ src/tests/test_hexadecimal.py
@@ -2,11 +2,11 @@ import pytest
 
 from ..ciphers.impl.hexadecimal import Hexadecimal
 
-class TestHexadecimal(unittest.TestCase):
+# class TestHexadecimal(unittest.TestCase):
 
-    @pytest.fixture(autouse=True)
-    def test_hexadecimal_encode(self):
-        message = "test"
-        hex = Hexadecimal(message)
-        result = hex.encode()
-        self.assertEqual(result, "74657374")
+    # @pytest.fixture(autouse=True)
+def test_hexadecimal_encode():
+    message = "test"
+    hex = Hexadecimal(message)
+    result = hex.encode()
+    assert result == "74657374"
